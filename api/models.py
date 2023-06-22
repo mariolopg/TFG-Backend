04ac7717ace29adc19657d1f7c3693350e74a794
Mario López González <mariolopezgonzalez2001@gmail.com>
2023-06-22 19:22:59 +0200

    create app for scraping

--- api/models.py
+++ api/models.py
@@ -1,20 +1,3 @@
 from django.db import models
-from django.core.validators import *
 
 # Create your models here.
-
-class CPU(models.Model):
-    id = models.AutoField(primary_key = True)
-    name = models.CharField(blank = False, unique=True, max_length=100)
-    socket = models.CharField(blank = False, max_length=100)
-    cores = models.IntegerField(validators=[MinValueValidator(1)])
-    threads = models.IntegerField(validators=[MinValueValidator(1)])
-
-    class Meta:
-        _name = ['name']
-        _socket = ['socket']
-        _cores = ['cores']
-        _threads = ['threads']
- 
-    def __str__(self):
-        return f"CPU: {self.name}"
--- app/urls.py
+++ app/urls.py
@@ -19,5 +19,5 @@ from api.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/scrap/<component>', ApiRoot)
+    path('api/scrap/<component>', Scrap)
 ]
